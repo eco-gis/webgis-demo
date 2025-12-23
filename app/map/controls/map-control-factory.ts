8b42c40b1cc26124b98d95ef4044ca06a5ce4b2f
lroost <luca.roost@bluewin.ch>
2025-12-23 16:45:49 +0100

    remove dependencies

--- app/map/controls/map-control-factory.ts
+++
@@ -1,6 +0,0 @@
-// map/controls/map-control-factory.ts
-import { ToggleControl } from "./toggle-control";
-
-export function createToggleControl(html: string, onClick: () => void) {
-	return new ToggleControl(onClick, html);
-}
